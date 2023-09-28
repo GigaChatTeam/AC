8543c77e32214fb2606e03635e76cd5697b22fd6
mister-proger <i@savnil.ru>
2023-09-28 19:54:16 +0300

    Wrapped mail confirmation functions in class

--- Management WebPages/server/helper/DBOperator.py
+++ Management WebPages/server/helper/DBOperator.py
@@ -5,48 +5,50 @@ import psycopg2
 connection = psycopg2.connect(host='localhost', port=5432, user='postgres', password='password')
 
 
-def save_token(id, token):
-    cursor = connection.cursor()
+class Emails:
+    @staticmethod
+    def save_token(id, token):
+        cursor = connection.cursor()
 
-    cursor.execute('''
-        INSERT INTO public.ttokens (client, token, hashed, extradition, intentions)
-        VALUES (%s, %s, FALSE, %s, ARRAY['CONFIRM', 'EMAIL'])
-    ''', (id, token, datetime.datetime.now()))
+        cursor.execute('''
+            INSERT INTO public.ttokens (client, token, hashed, extradition, intentions)
+            VALUES (%s, %s, FALSE, %s, ARRAY['CONFIRM', 'EMAIL'])
+        ''', (id, token, datetime.datetime.now()))
 
-    connection.commit()
+        connection.commit()
 
+    @staticmethod
+    def verify_token(id, token):
+        cursor = connection.cursor()
 
-def verify_token(id, token):
-    cursor = connection.cursor()
+        cursor.execute('''
+            SELECT token
+            FROM public.ttokens
+            WHERE
+                client = 1 AND
+                intentions = ARRAY['CONFIRM', 'EMAIL'] AND
+                CURRENT_TIMESTAMP - extradition <= INTERVAL '4 hours' AND
+                blocked IS NULL
+        ''', (id,))
 
-    cursor.execute('''
-        SELECT token
-        FROM public.ttokens
-        WHERE
-            client = 1 AND
-            intentions = ARRAY['CONFIRM', 'EMAIL'] AND
-            CURRENT_TIMESTAMP - extradition <= INTERVAL '4 hours' AND
-            blocked IS NULL
-    ''', (id,))
+        if token in cursor.fetchall():
+            return True
+        else:
+            return False
 
-    if token in cursor.fetchall():
-        return True
-    else:
-        return False
+    @staticmethod
+    def close_tokens(id):
+        cursor = connection.cursor()
 
+        cursor.execute('''
+            UPDATE public.ttokens
+            SET
+                blocked = %s,
+                reason = 'verification has already been made'
+            WHERE
+                client = %s AND
+                intentions = ARRAY['CONFIRM', 'EMAIL'] AND
+                blocked IS NULL
+        ''', (datetime.datetime.now(), id))
 
-def close_tokens(id):
-    cursor = connection.cursor()
-
-    cursor.execute('''
-        UPDATE public.ttokens
-        SET
-            blocked = %s,
-            reason = 'verification has already been made'
-        WHERE
-            client = %s AND
-            intentions = ARRAY['CONFIRM', 'EMAIL'] AND
-            blocked IS NULL
-    ''', (datetime.datetime.now(), id))
-
-    connection.commit()
+        connection.commit()
