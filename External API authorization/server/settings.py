7d89e3196660a0b65e688b518657b06e33195ec8
mister-proger <i@savnil.ru>
2023-11-11 10:58:55 +0300

    Update settings.py

--- External API authorization/server/settings.py
+++ External API authorization/server/settings.py
@@ -1,8 +1,11 @@
-ALLOWED_HOSTS = ['*']
-ROOT_URLCONF = 'server.urls'
+from decouple import config
 
-SECRET_KEY = r'j(y0++vg$zgf9n#$b(&e9-w)sa#5+z41&dzcsz4a_t4)p=8f@e'
-DEBUG = True
+
+SECRET_KEY = config("SECRET_KEY")
+DEBUG = config("DEBUG", cast=bool)
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=list[str])
+
+ROOT_URLCONF = 'server.urls'
 
 DATABASES = {
     'default': {
