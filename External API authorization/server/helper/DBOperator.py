02aa21ed2dc268cf4f3e6953bd0d0d7fbbf455b4
mister-proger <i@savnil.ru>
2023-12-10 02:15:47 +0300

    Moved settings to configuration files

--- External API authorization/server/helper/DBOperator.py
+++ External API authorization/server/helper/DBOperator.py
@@ -3,8 +3,15 @@ import datetime
 import psycopg2
 
 from . import generator
+from .. import settings
 
-connection = psycopg2.connect(host='localhost', port=5432, user='postgres', password='password')
+connection = psycopg2.connect(
+    host=settings.DATABASES['default']['HOST'],
+    port=settings.DATABASES['default']['PORT'],
+    user=settings.DATABASES['default']['USER'],
+    password=settings.DATABASES['default']['PASSWORD'],
+    application_name=settings.DATABASES['default']['APPLICATION']
+)
 connection.autocommit = True
 
 
--- External API authorization/server/settings.py
+++ External API authorization/server/settings.py
@@ -6,14 +6,17 @@ ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=list[str])
 
 ROOT_URLCONF = 'server.urls'
 
+SERVER_ID = config("SERVER_ID")
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': '',
-        'USER': 'postgres',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': config("DB_NAME"),
+        'USER': config("DB_USER"),
+        'PASSWORD': config("DB_PASSWORD"),
+        'HOST': config("DB_HOST"),
+        'PORT': config("DB_PORT", cast=int),
+        'APPLICATION': config("APPLICATION").format(id=SERVER_ID),
     }
 }
 
